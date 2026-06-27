fabb45af3eee6626fd4be4b271ea3ae8afe505f5
Alex Chevrier <chevrier.alex@gmail.com>
2026-06-27 19:02:10 +0800

    chore: remove temporary benchmarking scripts accidentally committed

--- fix_h132.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open('autumn-compiler-plugin/src/main/kotlin/dev/autumn/compiler/TopologySynthesisTransformer.kt', 'r') as f:
-    text = f.read()
-
-# To cleanly rewrite the generation block...

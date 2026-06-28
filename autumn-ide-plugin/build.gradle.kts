3df515557b4d3dc3e3f8f5e2a99318c506246b4a
Alex Chevrier <chevrier.alex@gmail.com>
2026-06-29 00:05:39 +0800

    fix(ide): apply kotlin serialization compiler plugin to decode architecture payload

--- autumn-ide-plugin/build.gradle.kts
+++ autumn-ide-plugin/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.intellij.platform") version "2.0.1"
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.21"
     id("org.jetbrains.compose") version "1.7.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
 }
 
 
